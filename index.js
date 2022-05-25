ef7d073795da5a0d9630a8e909c7c69743f2a2e0
Pat M <92064826+PatrickMarinich@users.noreply.github.com>
2022-05-24 21:42:40 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+//code from https://contactmentor.com/build-website-visitor-counter-javascript/
+
+var countContain = document.querySelector(".website-counter");
+var visitCount = localStorage.getItem("page_view");
+
+if(visitCount){
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view",visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view",1);
+} 
+countContain.innerHTML = visitCount;
